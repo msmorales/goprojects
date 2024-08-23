7ec44bbf1926a58449e3cc56ab121325b2dd2073
Michael Morales <maicol_sneyder@hotmail.com>
2024-08-22 20:02:01 -0500

    Siguiente commit

--- ejercicios/ejercicio2.go
+++ ejercicios/ejercicio2.go
@@ -11,23 +11,25 @@ var num int
 var err error
 
 func TablaDeMultiplicar() {
-
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Ingrese número: ")
-	// Leer datos del teclado
-	if scanner.Scan() {
-		// Convertir el texto a un número
-		num, err = strconv.Atoi(scanner.Text())
-		// Validar si hay un error
-		if err != nil {
-			// Mostrar mensaje de error
-			continue
-			//panic("El dato ingresado es incorrecto" + err.Error())
-		}
-		else{
-			break
+	for {
+		fmt.Println("Ingrese número: ")
+		// Leer datos del teclado
+		if scanner.Scan() {
+			// Convertir el texto a un número
+			num, err = strconv.Atoi(scanner.Text())
+			// Validar si hay un error
+			if err != nil {
+				// Mostrar mensaje de error
+				continue
+				//panic("El dato ingresado es incorrecto" + err.Error())
+			} else {
+				break
+			}
 		}
 	}
+
+	fmt.Println("Tabla de multiplicar del", num)
 
 	for i := 1; i <= 10; i++ {
 		fmt.Println(num, "x", i, "=", num*i)
